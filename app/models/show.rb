59aea5b2acc00357c5a54de36b34bd4de00ba0dc
Mariusz <mariuszbia@yahoo.com>
2022-08-13 21:29:30 -0500

    Completed assignment.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        self.characters.collect { |character| "#{character.actor.first_name} #{character.actor.last_name}"}
+    end
 end
